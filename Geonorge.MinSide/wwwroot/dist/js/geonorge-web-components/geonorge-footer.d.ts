c2f287cf081c317b1fc75987ebb06a2d7eda0386
Dag Olav Dahle <dagolav@arkitektum.no>
2025-03-17 14:08:13 +0100

    Update menu/userinfo.

--- Geonorge.MinSide/wwwroot/dist/js/geonorge-web-components/geonorge-footer.d.ts
+++ Geonorge.MinSide/wwwroot/dist/js/geonorge-web-components/geonorge-footer.d.ts
@@ -17,16 +17,21 @@ export declare class GeonorgeFooter extends CustomElement {
     version: string;
     language: string;
     accessibilitystatementurl: string;
+    privacyurl: string;
     hideaccessibilitystatementlink: boolean;
+    hideprivacylink: boolean;
     constructor();
     setup(options?: StandardButtonOptions): void;
-    addLinkListContent(linkListElement: HTMLUListElement, language: string, environment: string, accessibilitystatementurl: string, hideaccessibilitystatementlink: boolean): HTMLUListElement;
+    addLinkListContent(linkListElement: HTMLUListElement, language: string, environment: string, accessibilitystatementurl: string, privacyurl: string, hideaccessibilitystatementlink: boolean, hideprivacylink: boolean): HTMLUListElement;
     getContactInfoText(language: string, environment: string): "\n            <p>\n            Telephone: +47 32 11 80 00<br>\n            <a title=\"post@norgedigitalt.no\" href=\"mailto:post@norgedigitalt.no\">post@norgedigitalt.no</a><br>\n            Org. nr.: 971 040 238\n            </p>\n            " | "\n            <p>\n            Telefon: 32 11 80 00<br>\n            <a title=\"post@norgedigitalt.no\" href=\"mailto:post@norgedigitalt.no\">post@norgedigitalt.no</a><br>\n            Org. nr.: 971 040 238\n            </p>\n            ";
     shouldHideAccessibilityStatementLink(hideaccessibilitystatementlink: any): boolean;
+    shouldHidePrivacyLink(hideprivacylink: any): boolean;
     renderLinkList(): void;
     connectedCallback(): void;
     languageChanged(): void;
     hideaccessibilitystatementlinkChanged(): void;
     accessibilitystatementurlChanged(): void;
+    privacyurlChanged(): void;
+    hideprivacylinkChanged(): void;
 }
 export {};
